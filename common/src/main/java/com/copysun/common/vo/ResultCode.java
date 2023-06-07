121cdab1458c736e6248f8428d2e00bda6c04e8d
Copy123 <CopySun123@qq.com>
2023-06-07 12:12:33 +0800

    公共

---
+++ common/src/main/java/com/copysun/common/vo/ResultCode.java
@@ -0,0 +1,26 @@
+package com.copysun.common.vo;
+
+import lombok.Getter;
+
+/**
+ * @Author copysun
+ * @Date: Create in 15:45 2022/6/21
+ * @Desc
+ */
+@Getter
+public enum ResultCode implements StatusCode{
+	SUCCESS(1000,"处理成功"),
+	FAILED(1001,"处理失败"),
+	VALIDATE_ERROR(1002, "参数校验失败"),
+	RESPONSE_PACK_ERROR(1003, "response返回包装失败"),
+	REMOTE_CALL(1004,"远程调用失败")
+	;
+    private final int code;
+	private final String msg;
+
+	ResultCode(int code, String msg){
+		this.code = code;
+		this.msg = msg;
+	}
+
+}
---
+++ common/src/main/java/com/copysun/common/vo/StatusCode.java
@@ -0,0 +1,12 @@
+package com.copysun.common.vo;
+
+/**
+ * @Author copysun
+ * @Date: Create in 15:43 2022/6/21
+ * @Desc
+ */
+public interface StatusCode {
+	int getCode();
+
+	String getMsg();
+}
